461f9345001622b0ee1388e4e05ac5161d2bc1fc
unknown <gerencialesaplicaciones@gmail.com>
2023-11-05 23:41:06 -0600

    Cambio de idioma a español para alertas del login

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'failed' => 'El usuario o la contraseña son incorrectos, intenta de nuevo.', # mensaje si no coinciden datos
+    'throttle' => 'Muchos intentos de login. Intenta de nuevo en :seconds segundos.', # mensaje si intenta loguearse muchas veces y falla
+];

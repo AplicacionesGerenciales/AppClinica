78a808ca37d0bcbe4f7c5a6f2be132bc7026321d
OSMANY CASCO <gerencialesaplicaciones@gmail.com>
2023-11-07 15:57:38 -0600

    Roles view_Controller Kernel_Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
+//Para la paginacion
+use Illuminate\Pagination\Paginator;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        Paginator::useBootstrap();
     }
 }

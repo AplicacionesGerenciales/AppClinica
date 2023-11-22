9489bd7afe60e33aebf6ea2d62fdf7ed4cbe8c21
scarleth-meli <147758262+scarleth-meli@users.noreply.github.com>
2023-11-22 08:46:19 -0600

    Inserción de exámenes directamente en la consulta

--- app/Http/Controllers/MedicalConsultationController.php
+++ app/Http/Controllers/MedicalConsultationController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\TypeExamination;
 use App\Models\Disease;
 use App\Models\Doctor;
+use App\Models\Examination;
 use App\Models\File;
 use App\Models\MedicalConsultation;
 use Illuminate\Http\Request;
@@ -20,12 +21,27 @@ class MedicalConsultationController extends Controller
         $doctor = Doctor::all();
         $file = File::all();
         $disease = Disease::all();
-        return view('medical-consultation.index', compact('medicalConsultations', 'doctor', 'file', 'disease'))->with('i', (request()->input('page', 1) - 1) * $medicalConsultations->perPage());
+        $type_examination = TypeExamination::all();
+        return view('medical-consultation.index', compact('medicalConsultations', 'doctor', 'file', 'disease', 'type_examination'))->with('i', (request()->input('page', 1) - 1) * $medicalConsultations->perPage());
     }
     public function store(Request $request)
     {
+
+        $idexamination = $request->input('type_examination_id');
+        $result = $request->input('result');
+        
         request()->validate(MedicalConsultation::$rules, MedicalConsultation::$messages);
-        MedicalConsultation::create($request->all());
+
+        $consultation = MedicalConsultation::create($request->all());
+        if($idexamination !== 2){
+            $idconsultation = $consultation->id;
+            $array = [
+                "result" => $result,
+                "type_examination_id" => $idexamination,
+                "medical_consultation_id" => $idconsultation
+            ];
+        }
+        Examination::create($array);
         return redirect()->route('medical-consultations.index')->with('mensaje', 'OkCreate.');
     }
     public function update(Request $request, $id)
